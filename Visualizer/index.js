b0ff53374dd77079783ba2fbe29de56e36708af8
Bertus Kruger <krugercreations@gmail.com>
2015-05-07 21:06:30 +1200

    Added Example code of Client/Server websocket coms

--- Visualizer/index.js
+++ Visualizer/index.js
@@ -2,16 +2,35 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// List serial ports
+var serialPort = require("serialport");
+serialPort.list(function (err, ports) {
+  ports.forEach(function(port) {
+    console.log(port.comName);
+    console.log(port.pnpId);
+    console.log(port.manufacturer);
+  });
+});
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
+  // Send Welcome
+  io.emit('WelcomeMessage','Welcome to "ME" server');
+
+  socket.on('WelcomeMessage', function(msg){
+    console.log('Client said:' + msg);
+  });
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 });
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
