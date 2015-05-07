37b97b3f85e764ca0c4a6ac81aa4fbc75f8dfb0a
Bertus Kruger <bertus.kruger@pertronic.co.nz>
2015-05-08 11:17:23 +1200

    Connected Serial Port and sending data to client

--- Visualizer/index.js
+++ Visualizer/index.js
@@ -3,7 +3,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 // List serial ports
-var serialPort = require("serialport");
+/*
+var serialPort = require('serialport');
 serialPort.list(function (err, ports) {
   ports.forEach(function(port) {
     console.log(port.comName);
@@ -11,6 +12,31 @@ serialPort.list(function (err, ports) {
     console.log(port.manufacturer);
   });
 });
+*/
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort;
+
+serialPort = new SerialPort("COM15", 
+	{
+		baudrate: 115200,
+		parser: serialport.parsers.readline("\n")
+	}
+);
+serialPort.on("open", function () {
+  console.log('open');
+  serialPort.on('data', function(data) {
+    console.log('data received: ' + data);
+    io.emit('serialData',data);
+
+  });
+  /*
+  serialPort.write("ls\n", function(err, results) {
+    console.log('err ' + err);
+    console.log('results ' + results);
+  });
+  */
+});
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
